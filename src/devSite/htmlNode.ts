d7ef106d6e5f45c46710ad022a9d2c5501b6ef94
ZuperZee <ole.kristian.kaaseth@gmail.com>
2022-08-04 13:47:35 +0200

    fix(dev): fix styles not updating withuot reload

--- src/devSite/htmlNode.ts
+++ src/devSite/htmlNode.ts
@@ -9,4 +9,21 @@ window.htmlNode = shadowContainer.attachShadow({ mode: "open" }) as HTMLNode;
 htmlNode.onpanelupdate = () => {
   // Do nothing
 };
-htmlNode.innerHTML = `<style>@import "/src/style.css"</style>${html}`;
+htmlNode.innerHTML = `<style></style>${html}`;
+
+// Copy the styles in the document head into the shadow DOM
+const stylesNodes: Node[] = [];
+const observer = new MutationObserver((mutations) => {
+  for (const mutation of mutations) {
+    if (mutation.target === document.head) {
+      for (const n of mutation.addedNodes) {
+        stylesNodes.push(n);
+      }
+    }
+  }
+
+  htmlNode.children[0].innerHTML = stylesNodes
+    .map((s) => s.textContent)
+    .join("");
+});
+observer.observe(document.head, { childList: true, subtree: true });
