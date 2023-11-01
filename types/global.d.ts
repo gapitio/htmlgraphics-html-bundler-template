16f42c960d5fd8f74fe5770e7f616683d4ca7319
Flesaker <flesakern@gmail.com>
2023-11-01 11:46:59 +0100

    refac: update types

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,4 +1,17 @@
-import type { PanelData } from "@grafana/data";
+import {
+  FieldDisplay,
+  FieldReducerInfo,
+  GrafanaTheme,
+  GrafanaTheme2,
+  PanelData,
+  PanelProps,
+  Registry,
+} from "@grafana/data";
+import {
+  getLocationSrv as getLocationSrvType,
+  getTemplateSrv as getTemplateSrvType,
+  LocationService,
+} from "@grafana/runtime";
 import type { OptionsInterface } from "./htmlgraphicsTypes/options";
 import type customPropertiesJSON from "../src/custom-properties.json";
 import type { HTMLNode } from "./htmlgraphicsTypes/htmlNode";
@@ -6,37 +19,130 @@ import type { GrafanaTheme } from "./htmlgraphicsTypes/theme";
 
 declare global {
   /**
-   * The HTML node from the HTML/SVG code.
-   *
-   * It's a shadow root https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_shadow_DOM.
+   * The [ShadowRoot](https://developer.mozilla.org/en-US/docs/Web/API/ShadowRoot) which contains the elements added in the HTML/SVG document.
    */
   const htmlNode: HTMLNode;
-  const customProperties: typeof customPropertiesJSON;
-
   /**
-   * Result set of panel queries
-   * https://grafana.com/docs/grafana/latest/packages_api/data/paneldata/.
+   * The parsed JSON object from the Custom properties option.
+   *
+   * @deprecated in favor of {@link customProperties}
+   */
+  const codeData: typeof customPropertiesJSON;
+  /**
+   * The parsed JSON object from the Custom properties option.
+   */
+  const customProperties: typeof codeData;
+  /**
+   * The PanelData interface passed into the panel by Grafana.
    */
   const data: PanelData;
-
   /**
-   * The panel options.
+   * The options object.
    */
   const options: OptionsInterface;
-
   /**
-   * Grafana theme.
-   * Here you can get the current theme, colors, sizes, ETC.
-   *
-   * https://grafana.com/docs/grafana/latest/packages_api/data/grafanatheme/
+   * The GrafanaTheme object. It stores the current theme (light/dark), colors used by grafana, ETC.
    */
   const theme: GrafanaTheme;
+  /**
+   * Used to retrieve the {@link TemplateSrv} that can be used to fetch available
+   * template variables.
+   *
+   */
+  const getTemplateSrv: typeof getTemplateSrvType;
+  /**
+   * Used to retrieve the {@link LocationSrv} that can be used to automatically navigate
+   * the user to a new place in Grafana.
+   *
+   * @deprecated in favor of {@link locationService} and will be removed in Grafana 9
+   */
+  const getLocationSrv: typeof getLocationSrvType;
+
+  const htmlGraphics: {
+    /**
+     * The [ShadowRoot](https://developer.mozilla.org/en-US/docs/Web/API/ShadowRoot) which contains the elements added in the HTML/SVG document.
+     */
+    htmlNode: typeof htmlNode;
+    /**
+     * The parsed JSON object from the Custom properties option.
+     *
+     * @deprecated in favor of {@link customProperties}
+     */
+    codeData: typeof codeData;
+    /**
+     * The parsed JSON object from the Custom properties option.
+     */
+    customProperties: typeof customProperties;
+    /**
+     * The PanelData interface passed into the panel by Grafana.
+     */
+    data: typeof data;
+    /**
+     * The options object.
+     */
+    options: typeof options;
+    /**
+     * The GrafanaTheme object. It stores the current theme (light/dark), colors used by grafana, ETC.
+     */
+    theme: typeof theme;
+    /**
+     * The new GrafanaTheme2 object introduced in Grafana v8. It stores the current theme (light/dark), colors used by grafana, ETC.
+     */
+    theme2: GrafanaTheme2;
+    /**
+     * Used to retrieve the {@link TemplateSrv} that can be used to fetch available
+     * template variables.
+     *
+     */
+    getTemplateSrv: typeof getTemplateSrv;
+    /**
+     * Used to retrieve the {@link LocationSrv} that can be used to automatically navigate
+     * the user to a new place in Grafana.
+     *
+     * @deprecated in favor of {@link locationService} and will be removed in Grafana 9
+     */
+    getLocationSrv: typeof getLocationSrv;
+    /**
+     * A wrapper to help work with browser location and history.
+     */
+    locationService: LocationService;
+    /**
+     * Containing all the props from the panel PanelProps.
+     */
+    props: PanelProps<OptionsInterface>;
+    /**
+     * The width of the panel
+     */
+    width: number;
+    /**
+     * The height of the panel
+     */
+    height: number;
+    /**
+     * Returns a list of reduced values.
+     */
+    getFieldDisplayValues: (
+      options: PopulatedGetFieldDisplayValuesOptions,
+    ) => FieldDisplay[];
+    /**
+     * List of reduced values.
+     */
+    fieldDisplayValues: FieldDisplay[];
+    /**
+     * A list of the reducers.
+     */
+    fieldReducers: Registry<FieldReducerInfo>;
+  };
 
   interface Window {
     htmlNode: typeof htmlNode;
+    codeData: typeof codeData;
     customProperties: typeof customProperties;
     data: typeof data;
     options: typeof options;
     theme: typeof theme;
+    getTemplateSrv: typeof getTemplateSrv;
+    getLocationSrv: typeof getLocationSrv;
+    htmlGraphics: typeof htmlGraphics;
   }
 }
